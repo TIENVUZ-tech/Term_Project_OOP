6f88de0c95751a3454e977baac7c6159f6dc9fe9
TIENVUZ-tech <tien3152005@gmail.com>
2025-10-01 01:03:09 +0700

    Viết nốt code của MovableObject.java và một chút của Paddle.java

--- src/main/java/com/DevChickens/Arkanoid/entities/MovableObject.java
+++ src/main/java/com/DevChickens/Arkanoid/entities/MovableObject.java
@@ -12,8 +12,43 @@ public abstract class MovableObject extends GameObject {
 
     /**
      * Phương thức khởi tạo MovableOject
+     * @param x (tọa độ x)
+     * @param y (toạ độ y)
+     * @param width (độ rộng)
+     * @param height (độ cao)
      * @param dx (tốc độ di chuyển theo trục x)
      * @param dy (tốc độ di chuyển theo chiều y)
      */
+    public MovableObject(double x, double y, double width, double height,
+     double dx, double dy) {
+        super(x, y, width, height);
+        this.dx = dx;
+        this.dy = dy;
+    }
+    
+    /** Phương thức getter và setter cho dx (tốc độ theo phương ngang) */
+    public void setDx(double dx) {
+        this.dx = dx;
+    }
 
+    public double getDx() {
+        return this.dx;
+    }
+
+    /** Phương thức getter và setter cho dy (tốc độ theo phương dọc) */
+    public void setDy(double dy) {
+        this.dy = dy;
+    }
+
+    public double getDy() {
+        return this.dy;
+    }
+
+    /**
+     * Phương thức move(). Di chuyển đối tượng dựa trên tốc độ di chuyển dx, dy.
+     * Phương thức move được để là abstract.
+     * Nghĩa là mọi lớp con kế thừa từ MovableObject bắt buộc phải cung cấp cách di chuyển riêng.
+     * @param none
+     */
+    public abstract void move();
 }
--- src/main/java/com/DevChickens/Arkanoid/entities/Paddle.java
+++ src/main/java/com/DevChickens/Arkanoid/entities/Paddle.java
@@ -1,4 +1,55 @@
 package com.DevChickens.Arkanoid.entities;
+/**
+ * Paddle (Kế thừa từ MovableObject): Thanh đỡ mà người chơi điều khiển.
+ * Thuộc tính: speed, currentPowerUp.
+ * Phương thức: moveLeft(), moveRight(), applyPowerUp(). 
+ */
 
-public class Paddle {
+import com.DevChickens.Arkanoid.entities.powerups.PowerUp;
+
+public class Paddle extends MovableObject {
+    /* Tốc độ di chuyển của Paddle theo phương ngang.
+     * với mỗi lần cập nhật thì vị trí của paddle thay đổi speed (pixel).
+     */
+    private double speed;
+    /* Tham chiếu đến PowerUp mà hiện tại paddle đang áp dụng */
+    private PowerUp currentPowerUp;
+
+    /**
+     * Phương thức khởi tạo Paddle
+     * @param x (tọa độ x)
+     * @param y (toạ độ y)
+     * @param width (độ rộng)
+     * @param height (độ cao)
+     * @param dx (tốc độ di chuyển theo trục x)
+     * @param dy (tốc độ di chuyển theo chiều y)
+     * @param speed (tốc độ di chuyển)
+     * @param currentPowerUp (PowerUp hiện tại mà paddle đang dùng)
+     */
+    public Paddle(double x, double y, double width, double height,
+     double dx, double dy, double speed, PowerUp currPowerUp) {
+        super(x, y, width, height, dx, dy);
+        this.speed = speed;
+        this.currentPowerUp = currPowerUp;
+     }
+
+    /** Phương thức getter và setter của speed (tốc độ)*/
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getSpeed() {
+        return this.speed;
+    }
+
+    /** Phương thức getter và setter của currentPowerUp (trạng thái PowerUp hiện tại của Paddle)*/
+    public void setCurrentPower(PowerUp currentPowerUp) {
+        this.currentPowerUp = currentPowerUp;
+    }
+
+    public PowerUp getCurrentPower() {
+        return this.currentPowerUp;
+    }
+
+    
 }
