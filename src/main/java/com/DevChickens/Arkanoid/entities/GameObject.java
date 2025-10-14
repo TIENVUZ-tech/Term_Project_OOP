189d5e4adbb5d303aeedfbc518747fbbb85fa10c
TIENVUZ-tech <tien3152005@gmail.com>
2025-10-15 00:45:08 +0700

    Fix(GameObject.java): delete method render()
    Xóa đi vì không cần dùng đến.

--- src/main/java/com/DevChickens/Arkanoid/entities/GameObject.java
+++ src/main/java/com/DevChickens/Arkanoid/entities/GameObject.java
@@ -76,7 +76,5 @@ public abstract class  GameObject {
      * Được gọi sau update() để hiển thị hình ảnh mới nhất.
      * Dùng đối tượng đồ hoạ (Graphics) để vẽ hình chữ nhật, ảnh sprite… tại vị trí đã cập nhật.
      */
-    public abstract void render();
-
     public abstract void render(Graphics g);
 }
