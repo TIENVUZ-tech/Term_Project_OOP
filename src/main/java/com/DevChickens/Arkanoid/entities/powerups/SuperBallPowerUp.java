a4e6c79c10f0f6625cb9a5c43ac7626d6591512c
TIENVUZ-tech <tien3152005@gmail.com>
2025-10-15 00:43:10 +0700

    Feat(SuperBallPowerUp.java): create class and write code
    Tạo và viết code cho SuperBallPowerUp.java (giúp đổi dạng bóng)

---
+++ src/main/java/com/DevChickens/Arkanoid/entities/powerups/SuperBallPowerUp.java
@@ -0,0 +1,34 @@
+package com.DevChickens.Arkanoid.entities.powerups;
+
+import java.awt.Graphics;
+
+import com.DevChickens.Arkanoid.entities.Ball;
+import com.DevChickens.Arkanoid.entities.Paddle;
+
+/**
+ * Loại PowerUp này sẽ làm cho bóng mạnh hơn và bóng sẽ gây 2 sát thương mỗi khi va chạm với Brick.
+ */
+public class SuperBallPowerUp extends PowerUp {
+    /**
+     * Constructor.
+     * @param x (tọa độ x).
+     * @param y (tọa độ y).
+     * @param type (kiểu powerUp)
+     * @param duration (thời gian tồn tại).
+     */
+    public SuperBallPowerUp(double x, double y, String type, int duration) {
+        super(x, y, type, duration);
+    }
+
+    @Override
+    public void applyEffect(Paddle paddle, Ball ball) {
+        ball.activateSuperBall(this.getDuration());
+        ball.update();
+    }
+
+    @Override
+    public void removeEffect(Paddle paddle, Ball ball) {}
+
+    @Override
+    public void render(Graphics g) {}
+}
